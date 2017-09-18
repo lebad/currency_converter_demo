b5f44c32470001dea4bd4094ce1fcbd71a24efa1
andrey <lebedac@gmail.com>
2017-09-19 00:39:23 +0300

    second controller

---
+++ revolutCurrencyConverterDemo/REVExchaneViewController.h
@@ -0,0 +1,13 @@
+//
+//  REVExchaneViewController.h
+//  revolutCurrencyConverterDemo
+//
+//  Created by andrey on 17/09/2017.
+//  Copyright © 2017 lebedac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface REVExchaneViewController : UIViewController
+
+@end
---
+++ revolutCurrencyConverterDemo/REVExchaneViewController.m
@@ -0,0 +1,22 @@
+//
+//  REVExchaneViewController.m
+//  revolutCurrencyConverterDemo
+//
+//  Created by andrey on 17/09/2017.
+//  Copyright © 2017 lebedac. All rights reserved.
+//
+
+#import "REVExchaneViewController.h"
+
+@interface REVExchaneViewController ()
+
+@end
+
+@implementation REVExchaneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
